7be37e9c8e12e88e8bca98fba1f96937b9912d55
AungKhantKyaw2 <japangatone2020@gmail.com>
2024-08-04 16:04:59 +0700

    Add files via upload

---
+++ java/JS/app.js
@@ -0,0 +1,123 @@
+// console.log("hi")
+
+// let first_name ="Aung"
+// let second_name ="Kyaw"
+
+// console.log(first_name+"asf"+second_name)
+
+// // string
+// let name ="Aung"
+
+// // number
+// // let age =23
+// let age  = "23"
+// console.log(age +2) //232   
+
+// let isMarried =false
+// console.log(isMarried)
+
+// let empty =null
+// console.log(empty)
+
+// let unknownVar;
+// console.log(unknownVar  )
+
+
+// let name = "Aung Khant Kyaw"
+
+// if (name == "Aung Khant Kyaw"){
+//     console.log("he is" + " "+name)
+// }
+// else{
+//     console.log("I dont know")
+// }
+
+
+// false condition
+// 1. 0
+// 2.false
+// 3."" or ' '(empty string)
+// 4.null 
+// 5. undefined
+
+// if("10"===10){
+//       console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// // let name = "Aung"
+// let name;
+// if(name){
+//     console.log("name")
+// }
+// else{
+//   console.log("no name")
+// }
+
+
+// let age = 23 
+// if (age >20 && age<30){
+
+//     console.log(true)
+
+// }
+// else{
+
+//     console.log(false)
+// }
+// and gate &&
+// let age = 23 
+// if (age >20 || age<20){
+
+//     console.log(true)
+
+// }
+// else{
+
+//     console.log(false)
+// }
+// or gate ||
+
+// function 
+// name()
+// console.log("Hi");
+// console.log("Hi");
+// console.log("Hi");
+// console.log("Hi");console.log("Hi");
+// function name(){
+
+    
+    // return "Dog"; 
+    // console.log("AUNG");  below return they will not show in console
+// }   
+// console.log(name());
+// name() ReferenceError: Cannot access 'name' before initialization
+
+// let name = function(){
+
+//     console.log("AUng")
+// }
+// name()
+// let name =()=>{             Another way
+//     console.log("AU1")
+// }
+// name()
+
+// let yourname =(name)=>{             
+//     console.log(name)
+// }
+// yourname("DOG")
+// name()
+// let name =(name,age)=>{             
+//     // console.log(name,age)
+//     return {name : name,age :age} ;
+// }
+
+// console.log(name("God",23))
+
+
+// in the function only have return  operation
+// var getname = yourname=>yourname;
+
+// console.log(getname("Aung Kyaw"))
---
+++ java/JS/app1.js
@@ -0,0 +1,7 @@
+// var getname = yourname=>yourname;
+
+// console.log(getname("Aung Kyaw"))
+// let my =()=>{
+//     console.log("Cao")
+// }
+// my()
