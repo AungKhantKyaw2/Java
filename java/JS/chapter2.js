041d35ae625beb53bdbe7e452f7687b969d2d1e8
AungKhantKyaw2 <japangatone2020@gmail.com>
2024-08-09 07:22:09 +0700

    chapter2.js

--- java/JS/chapter2.js
+++ java/JS/chapter2.js
@@ -15,9 +15,9 @@
 // let request = new XMLHttpRequest();
 
 // request.addEventListener("readystatechange",()=>{
-//     if(request.readyState===4){
+//     if(request.readyState===1){
 
-//     console.log(request.responseText);
+//     console.log(request.status);
 //     }
 // })
 
@@ -25,21 +25,85 @@
 
 // request.send();
 
+// //using call back
+// let getTodos = (callback)=>{
 
-let request = new XMLHttpRequest();
+//         let request = new XMLHttpRequest();
+         
+//         request.addEventListener("readystatechange",()=>{
+//              if(request.readyState=== 4 && request.status===200){
+               
+//             // console.log(request.responseText);
+//                  callback(request.responseText,undefined )   
+//             }
+//              else if (request.status===404)
+//              {
+//                 callback(undefined,"Something went wrong")
+//              }
+// })
+// request.open("GET","https://jsonplaceholder.typicode.com/todos/");
 
-request.addEventListener("readystatechange",()=>{
-    if(request.readyState===4 && request.status===200){
+// request.send();
+// }
+// getTodos((data,err)=>{
 
-    console.log(request.responseText);
+//     console.log(data,err)
+// })
+
+
+let getTodos = (resource,callback)=>{
+
+            let request = new XMLHttpRequest();
+             
+            request.addEventListener("readystatechange",()=>{
+                 if(request.readyState=== 4 && request.status===200){
+                let datas =JSON.parse(request.responseText);
+                // console.log(request.responseText);
+                     callback(datas,undefined )   
+                }
+                 else if (request.status===404)
+                 {
+                    callback(undefined,"Something went wrong")
+                 }
+                 else  if (request.status === 304) {
+                    callback(undefined, "Resource not modified (304)");}
+    })
+    request.open("GET",resource);
+    
+    request.send();
+
+// console.log("I am first")
+}
+// getTodos((data,err)=>{
+
+//     console.log(data,err)
+// })
+getTodos("JS/aung.json",(data,err)=>{
+    if(data){
+        console.log(data);
+        getTodos("JS/aung1.json",(data,err)=>{
+        if(data){
+        
+            console.log(data);  
+       
+        
     }
-    else if(request.status===404){
-        console.log("Error Http")
+    
+    else{
+        console.log(err)
+    }   
+    })
+    }
+    else{
+        console.log(err)
     }
 })
 
-request.open("GET","https://jsonplaceholder.typicode.com/todos/");
-
-request.send();
-
-console.log("I am first")
+getTodos("JS/aung2.json",(data,err)=>{
+       if(data){
+        console.log(data);
+       }
+       else{
+        console.log(err)
+       }
+}) 
