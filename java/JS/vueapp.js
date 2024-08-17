462726c00a2250c1e4ffa25b144522fef3a23a74
AungKhantKyaw2 <japangatone2020@gmail.com>
2024-08-17 17:15:22 +0700

    vueapp.js

--- java/JS/vueapp.js
+++ java/JS/vueapp.js
@@ -1,17 +1,71 @@
+let i=0;
 const app =Vue.createApp({
     // template: "<h1> hi there</h1>"
     data(){
         return{
-            title : "Aung God",
-            author : "God Of data",
-            age :20,
-            showbook : true,
+            books:[{
+                title : "Aung God",
+                author : "God Of data1",
+                age :20,
+                img : 'JS/img/boost.jpeg',
+                isFav: false
+            },
+            {
+                title : "Aung God2",
+                author : "God Of data2",
+                age :20,
+                img :'JS/img/git.png',
+                isFav: true
+            },  
+            {
+                title : "Aung God3",
+                author : "God Of data3",
+                age :20,
+                img :'JS/img/php.png',
+                isFav : true
+            },
+           
+        ],
+        showbook : true,
+        link :'https://www.perplexity.ai/search/how-to-write-for-loop-in-java-6jbfmgJZRtewa.ZAnNdNwQ'
+            
         }
     },
     methods:{
          IncreaseAge(){
             this.age++;
+         },
+         mousemovehandler(event,data){
+             console.log('mouse is working',event.type,data);
+         },
+
+         mouseleave(event,data){
+        
+          i++;
+          console.log(`mouse leaving ${i}`,event.type,data);  
+         },
+         dbclick(event,data){
+            console.log("double click",event.type,data);
+         },
+         clickcolorshow(book){
+            
+            // console.log(bs);
+            book.isFav = ! book.isFav;
+            
+            
          }
+
+    },  
+    computed:{
+          filterbook(){
+            return this.books.filter(data=>{
+                return  data.isFav;
+
+            });
+            
+          } 
+
+
     }
 });
 app.mount('#app');
